3bc7b468b9cbbb477a57064106425c7bc5d6ba46
tyage <namatyage@gmail.com>
2014-10-09 13:30:52 +0900

    migrate db

--- sample-app/db/schema.rb
+++ sample-app/db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141009042853) do
+
+  create_table "buyers", force: true do |t|
+    t.string   "email"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
