a168eede43ba8f3319b943413ebfdd2ecc7c9335
tyage <namatyage@gmail.com>
2014-10-10 13:49:57 +0900

    removed illegal tests of category

--- sample-app/test/controllers/categories_controller_test.rb
+++ sample-app/test/controllers/categories_controller_test.rb
@@ -11,39 +11,8 @@ class CategoriesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:categories)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create category" do
-    assert_difference('Category.count') do
-      post :create, category: { name: @category.name }
-    end
-
-    assert_redirected_to category_path(assigns(:category))
-  end
-
   test "should show category" do
     get :show, id: @category
     assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @category
-    assert_response :success
-  end
-
-  test "should update category" do
-    patch :update, id: @category, category: { name: @category.name }
-    assert_redirected_to category_path(assigns(:category))
-  end
-
-  test "should destroy category" do
-    assert_difference('Category.count', -1) do
-      delete :destroy, id: @category
-    end
-
-    assert_redirected_to categories_path
   end
 end
