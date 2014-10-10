5376dea12e9c1db3e184f5911769b3fd4b9bea16
tyage <namatyage@gmail.com>
2014-10-10 11:12:38 +0900

    不要なactionを削除

--- sample-app/app/controllers/sellers_controller.rb
+++ sample-app/app/controllers/sellers_controller.rb
@@ -1,5 +1,5 @@
 class SellersController < ApplicationController
-  before_action :set_seller, only: [:show, :destroy, :products]
+  before_action :set_seller, only: [:show, :products]
 
   # GET /sellers
   # GET /sellers.json
@@ -10,56 +10,6 @@ class SellersController < ApplicationController
   # GET /sellers/1
   # GET /sellers/1.json
   def show
-  end
-
-  # GET /sellers/new
-  def new
-    @seller = Seller.new
-  end
-
-  # GET /sellers/1/edit
-  def edit
-    @seller = current_seller
-  end
-
-  # POST /sellers
-  # POST /sellers.json
-  def create
-    @seller = Seller.new(seller_params)
-
-    respond_to do |format|
-      if @seller.save
-        format.html { redirect_to @seller, notice: 'Seller was successfully created.' }
-        format.json { render :show, status: :created, location: @seller }
-      else
-        format.html { render :new }
-        format.json { render json: @seller.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /sellers/1
-  # PATCH/PUT /sellers/1.json
-  def update
-    respond_to do |format|
-      if @seller.update(seller_params)
-        format.html { redirect_to @seller, notice: 'Seller was successfully updated.' }
-        format.json { render :show, status: :ok, location: @seller }
-      else
-        format.html { render :edit }
-        format.json { render json: @seller.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /sellers/1
-  # DELETE /sellers/1.json
-  def destroy
-    @seller.destroy
-    respond_to do |format|
-      format.html { redirect_to sellers_url, notice: 'Seller was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   # GET /sellers/1/products
