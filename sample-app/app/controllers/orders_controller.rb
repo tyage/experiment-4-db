a439a3aaab570ded79679c59fc787b12e52aa303
tyage <namatyage@gmail.com>
2014-10-10 12:43:30 +0900

    購入後、購入一覧ページへ

--- sample-app/app/controllers/orders_controller.rb
+++ sample-app/app/controllers/orders_controller.rb
@@ -21,7 +21,7 @@ class OrdersController < ApplicationController
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        format.html { redirect_to buyer_orders_path, notice: 'Order was successfully created.' }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
