5d00e023cbd28408bdda39183c1a1f0dfd082331
tyage <namatyage@gmail.com>
2014-10-10 13:07:32 +0900

    added seeds

--- sample-app/db/seeds.rb
+++ sample-app/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = %w(本 食料品 電化製品)
+existing_categories = Category.all.pluck(:name)
+categories.each do |c|
+  unless existing_categories.include?(c)
+    Category.create( name: c )
+  end
+end
