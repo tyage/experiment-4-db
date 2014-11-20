3f139c541321b5e93e41148c797007dd736de7de
tyage <namatyage@gmail.com>
2014-11-20 14:04:04 +0900

    入力しようとすると消えるように(うっとうしい

--- sample-app/app/assets/javascripts/animated-input.js
+++ sample-app/app/assets/javascripts/animated-input.js
@@ -1,18 +1,14 @@
 // うっとうしいアニメーション
 $(function() {
   $('input').each(function() {
-    var width = $(this).outerWidth();
-    var height = $(this).outerHeight();
     $(this).on('focus', function() {
       $(this).animate({
-        width: width * 1.5,
-        height: height * 1.5
-      });
+        opacity: 0
+      }, 300);
     });
     $(this).on('blur', function() {
       $(this).animate({
-        width: width,
-        height: height
+        opacity: 1
       });
     })
   });
